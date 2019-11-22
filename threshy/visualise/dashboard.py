e6b6c8e7b0ec357369dd103991de8778e2087c0f
Zac Brannelly <101668878@student.swin.edu.au>
2019-11-22 13:23:35 +1100

    QUALITY: Clean up file structure

--- threshy/visualise/dashboard.py
+++ threshy/visualise/dashboard.py
@@ -3,5 +3,5 @@ import tornado.web
 
 class Dashboard(tornado.web.RequestHandler):
     def get(self):
-        with open(os.path.join(os.path.dirname(__file__), "visualise_web_app.html"), 'rb') as f:
+        with open(os.path.join(os.path.dirname(__file__), "html", "visualise_web_app.html"), 'rb') as f:
             self.write(f.read())
