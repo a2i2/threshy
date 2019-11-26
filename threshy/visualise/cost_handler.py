4a586dd3f4573e64bf08a182d806bfb212c72a5a
Zac Brannelly <101668878@student.swin.edu.au>
2019-11-26 14:08:21 +1100

    QUALITY: Move cost functions from request handler

--- threshy/visualise/cost_handler.py
+++ threshy/visualise/cost_handler.py
@@ -2,18 +2,8 @@ import json
 import numpy as np
 import tornado.web
 
+from .cost import calculate_all_costs
 from .visualise_classifier import summarise_results
-
-def calculate_cost(predicted_matrix, cost_weights, portion_size, estimate_size):
-    cost_per_prediction = np.array(cost_weights) / portion_size
-    scaled_matrix = np.round(predicted_matrix.astype(np.double) / predicted_matrix.sum() * estimate_size)
-    return scaled_matrix * cost_per_prediction
-
-def calculate_all_costs(all_matrices, cost_weights, portion_size, estimate_size):
-    cost_results = np.zeros((len(all_matrices), 3, 3))
-    for i, a in enumerate(all_matrices):
-        cost_results[i] = calculate_cost(a, cost_weights, portion_size, estimate_size)
-    return cost_results
 
 class CostHandler(tornado.web.RequestHandler):
     def post(self):
