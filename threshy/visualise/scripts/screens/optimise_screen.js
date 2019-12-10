51b7ab379f9bdc7037c4a96e4c495ae69627f2e2
Zac Brannelly <101668878@student.swin.edu.au>
2019-12-10 14:29:01 +1100

    ENHANCE: Disable navigating while optimising

--- threshy/visualise/scripts/screens/optimise_screen.js
+++ threshy/visualise/scripts/screens/optimise_screen.js
@@ -268,13 +268,13 @@ const optimiseScreen = {
                 <div class="level-left"></div>
                 <div class="level-right">
                     <div class="level-item">
-                        <button v-on:click="onNext" class="button is-info">
+                        <button v-on:click="onNext" class="button is-info" :disabled="isOptimising">
                             <span class="icon"><i class="fas fa-arrow-circle-right"></i></span>
                             <span>Next</span>
                         </button>
                     </div>
                     <div class="level-item">
-                        <button v-on:click="$emit('screen-change', 'export')" class="button is-info">
+                        <button v-on:click="$emit('screen-change', 'export')" class="button is-info" :disabled="isOptimising">
                             <span class="icon"><i class="fas fa-arrow-circle-right"></i></span>
                             <span>Export</span>
                         </button>
